393f1d2cd2dbd538017386cccfc919bacaf878d2
GJ Tiquia <gershom.tiquia@gmail.com>
2022-03-13 02:23:50 +0800

    space for upward thrust relative to PlayerCube, follow camera also in right rotation

--- Assets/Scripts/FlightControl.cs
+++ Assets/Scripts/FlightControl.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class FlightControl : MonoBehaviour
 {
+    [SerializeField] private Rigidbody rgbd;
+    [SerializeField] private float thrust = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Jump
+        if (Input.GetButton("Jump"))
+        {
+            Debug.Log("Space");
+            rgbd.AddForce(transform.localEulerAngles.normalized * Physics.gravity.magnitude * thrust);
+        }
+
+        //
+
     }
 }
